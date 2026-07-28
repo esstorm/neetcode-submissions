231765947f85de47bec44949fe33574ba99e1285
Esteban Torres <e.torres.menendez@gmail.com>
2026-07-28 21:06:37 +0100

    Add: evaluate-reverse-polish-notation - submission-0

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-0.py
@@ -0,0 +1,31 @@
+from collections import deque
+from functools import reduce
+from operator import add, sub, mul, truediv
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        
+        tokens = deque(tokens)
+        stack = []
+
+        while tokens:
+            token = tokens.popleft()
+
+            if token not in ("+", "-", "*", "/"):
+               stack.append(token) 
+               continue
+
+            operator = {
+                "+": add,
+                "-": sub,
+                "*": mul,
+                "/": truediv
+            }[token]
+
+            right = stack.pop()
+            left = stack.pop()
+            stack.append(reduce(operator, [int(left), int(right)]))
+
+        return int(stack.pop())
+
+
